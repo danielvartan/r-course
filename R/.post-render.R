af6111ccba7e094aba04324314b7c9d8803b5ceb
Daniel Vartanian <danielvartan@proton.me>
2026-02-02 04:08:54 -0300

    Add index-speaker.html

--- R/.post-render.R
+++ R/.post-render.R
@@ -10,6 +10,15 @@ file <- here("docs", "images", "og-image.png")
 
 file_copy(here("images", "og-image.png"), file, overwrite = TRUE)
 
+# Move `index-speaker.html` to `./docs` -----
+
+if (file.exists(here("index-speaker.html"))) {
+  file_move(
+    here("index-speaker.html"),
+    here("docs", "index-speaker.html")
+  )
+}
+
 # Check If the Script Ran Successfully -----
 
 beep(1)
